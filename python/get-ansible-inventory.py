40b64ec19c234c4c0ef38892660c2d03ebc95118
root <root@controlplane1.top.oranus.com>
2023-02-27 11:14:56 +0000

    feat: added user names for ansible

--- python/get-ansible-inventory.py
+++ python/get-ansible-inventory.py
@@ -52,7 +52,7 @@ class Inventory(object):
       jsHostvars[nodeName] = {
         'ansible_host': platform_nodes_controlplanes[nodeName].get("ip"),
         'ansible_fqcn': nodeName + "." + platform_name + "." + platform_domain,
-        'ansible_user': 'admin',
+        'ansible_user': platform_nodes_controlplanes[nodeName].get("user"),
         'ansible_ssh_common_args': '-o StrictHostKeyChecking=no',
         'ansible_ssh_timeout': '60'
       }
@@ -65,7 +65,7 @@ class Inventory(object):
         jsHostvars[nodeName] = {
           'ansible_host': platform_nodes_workers[nodeName].get("ip"),
           'ansible_fqcn': nodeName + "." + platform_name + "." + platform_domain,
-          'ansible_user': 'admin',
+          'ansible_user': platform_nodes_workers[nodeName].get("user"),
           'ansible_ssh_common_args': '-o StrictHostKeyChecking=no',
           'ansible_ssh_timeout': '60'
         }
